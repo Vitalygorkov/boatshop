8c5a7f27a4cd34a4860c554aded078731d70cb56
Vitalygorkov <vet55omsk@gmail.com>
2022-02-17 01:28:32 +0600

    create models

--- neptun55/shop/admin.py
+++ neptun55/shop/admin.py
@@ -1,25 +1,7 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from .models import Type_boat, Color, Boat, Photo_boat, Category
+from .models import Type_boat, Color, Boat, Photo_boat, Category, Type_bottom, Manufacturer, Reviews,VideosBoats
 
-
-class Photo_boatInline(admin.StackedInline):
-    model = Photo_boat
-
-@admin.register(Boat)
-class BoatAdmin(admin.ModelAdmin):
-    list_display = ("boat_model", "boat_manufacturer", "type_boat", "type_motor", "price", )
-    list_display_links = ("boat_model",)
-    list_filter = ("boat_manufacturer", "type_boat", "type_motor", "price", "boat_model")
-    search_fields = ("boat_model","boat_manufacturer")
-    inlines = [Photo_boatInline]
-    # readonly_fields = ('get_image',) #вывод фотки в поле редактирования
-
-    #функция Для вывода картинки в админ панели
-    def get_image(self, obj):
-        return mark_safe(f'<img src={obj.image.url} width="160 height="180"')
-
-    get_image.short_description = "Изображение"
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -27,8 +9,65 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ("name",)
 
 
+class ReviewInline(admin.StackedInline):
+    """Отзывы на странице лодки"""
+    fields = ("name", "email", "text", "parent")
+    model = Reviews
+    extra = 1
+    # readonly_fields = ("name", "email")
+
+class Photo_boatInline(admin.StackedInline):
+    model = Photo_boat
+    extra = 1
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="320" height="240"')
+
+    get_image.short_description = "Изображение"
+
+class VideosInline(admin.StackedInline):
+    model = VideosBoats
+    extra = 1
+
+@admin.register(Boat)
+class BoatAdmin(admin.ModelAdmin):
+    list_display = ("boat_model", "boat_manufacturer", "type_boat", "type_motor", "price", )
+    list_display_links = ("boat_model",)
+    list_filter = ("boat_manufacturer", "type_boat", "type_motor", "price", "boat_model")
+    search_fields = ("boat_model","boat_manufacturer")
+    inlines = [VideosInline, Photo_boatInline, ReviewInline,]
+    save_on_top = True
+    save_as = True
+
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    """Отзывы к лодке"""
+    list_display = ("name", "email", "parent", "product", "id")
+    # readonly_fields = ("name", "email")
+
+@admin.register(Photo_boat)
+class Photo_boatAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "get_image", "boat",)
+    list_display_links = ("title",)
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="320" height="240"')
+
+    get_image.short_description = "Изображение"
+
+@admin.register(VideosBoats)
+class VideosAdmin(admin.ModelAdmin):
+    list_display = ("video", "boat")
 
 admin.site.register(Type_boat)
+admin.site.register(Manufacturer)
+admin.site.register(Type_bottom)
+
+
 # admin.site.register(Boat, BoatAdmin)
 admin.site.register(Color)
 
--- neptun55/shop/models.py
+++ neptun55/shop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField("Категория",max_length=150)
     description = models.TextField("Описание")
     url = models.SlugField(max_length=160, unique=True, verbose_name='слаг на английском, например "Category"')
 
@@ -14,7 +14,7 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
 
 class Type_boat(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField("Тип лодки", max_length=50)
 
     def __str__(self):
         return self.name
@@ -22,8 +22,27 @@ class Type_boat(models.Model):
         verbose_name = 'Тип лодки'
         verbose_name_plural = 'Типы лодок'
 
+class Type_bottom(models.Model):
+    name = models.CharField("Тип дна", max_length=50)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Тип дна'
+        verbose_name_plural = 'Типы дна'
+
+class Manufacturer(models.Model):
+    name = models.CharField("Производитель",max_length=50)
+    description = models.TextField("Описание")
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Производитель'
+        verbose_name_plural = 'Производители'
+
 class Color(models.Model):
-    name = models.CharField(max_length=12)
+    name = models.CharField("Цвет", max_length=50, unique= True)
 
     def __str__(self):
         return self.name
@@ -32,13 +51,14 @@ class Color(models.Model):
         verbose_name_plural = 'Цвета'
 
 class Boat(models.Model):
-    boat_model = models.CharField('Модель', max_length=150) # название
-    boat_manufacturer = models.CharField('Производитель',max_length=30) # производитель лодки.
+    boat_model = models.CharField('Модель', max_length=150, unique= True) # название
+    boat_manufacturer = models.ForeignKey(Manufacturer, verbose_name="Производитель", on_delete=models.SET_NULL, null=True) # производитель лодки.
     description = models.TextField('Описание', blank=True) # описание
-    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True) # Категория
     price = models.IntegerField('Цена', blank=True, null=True) # цена
 
     type_boat = models.ForeignKey(Type_boat, on_delete=models.PROTECT, verbose_name='Тип лодки') # тип лодки гребная или моторная
+    type_bottom = models.ForeignKey(Type_bottom, on_delete=models.PROTECT, verbose_name='Тип дна')  # тип л
     TYPE_MOTORS = (
         ('VINT', 'Винт'),
         ('VODOMET', 'Водомет'),
@@ -48,12 +68,14 @@ class Boat(models.Model):
     # transom =  # транец  Вклеенный,Крепление под навесной,Нет
     # floor =  # Надувное днище низкого давления,Надувное многобаллонное высокого давления,ПВХ,ПВХ+разборный пайол
     # series =  # серия, модельный ряд
-    color = models.CharField("Цвет лодки", max_length=10, blank=True) # цвет
+    color = models.ForeignKey(Color, verbose_name= "Цвет лодки", on_delete=models.SET_NULL, null=True, blank= True) # цвет
     length = models.IntegerField("Длинна см", blank=True,null=True) # длинна
     width = models.IntegerField("Ширина см", blank=True,null=True) # ширина
     cockpit_length = models.IntegerField("Длинна кокпита см", blank=True,null=True) # длинна кокпита см
     cockpit_width = models.IntegerField("Ширина кокпита см", blank=True, null=True) # ширина кокпита см
-    cylinder_diameter = models.IntegerField("Диаметр баллонов см", blank=True, null=True) # Диаметр баллонов см
+    cylinder_diameter = models.IntegerField("Диаметр борта см", blank=True, null=True) # Диаметр баллонов см
+    fabric_thickness_bottom = models.IntegerField("Плотность ткани дна (г/м²)", blank=True, null=True)  # Диаметр
+    fabric_thickness_side = models.IntegerField("Плотность ткани борта (г/м²)", blank=True, null=True)  # Диаметр
     inflatable_compartments = models.IntegerField("Количество надувных отсеков", blank=True, null=True) # Количество надувных отсеков
     load_capacity = models.IntegerField("Грузоподъемность кг", blank=True, null=True) # грузоподъемность кг
     passenger_capacity = models.IntegerField("Пассажировместимость чел", blank=True, null=True) # пассажировместимость чел
@@ -61,9 +83,8 @@ class Boat(models.Model):
     boat_weight = models.IntegerField("Вес лодки кг", blank=True, null=True) # вес лодки кг
     complete_set_weight = models.IntegerField("Вес полного комплекта кг", blank=True, null=True) # Вес полного комплекта кг
     bulwark = models.BooleanField("Фальшборт", default=False, blank=True) # фальшборт по дефолту нет.
-    payol = models.BooleanField("Пайол", default=False, blank=True) # Пайольная\беспайольная по дефолту да
     keel = models.BooleanField("Киль", default=False, blank=True) # Киль есть\нет, по дефолту нет
-    image = models.ImageField('Изображение', upload_to="boats/",blank=True) # фото  https://qna.habr.com/q/218059
+    # image = models.ImageField('Изображение', upload_to="boats/",blank=True) # фото  https://qna.habr.com/q/218059
     # видео
 
 
@@ -86,3 +107,27 @@ class Photo_boat(models.Model):
     class Meta:
         verbose_name = "Фото лодки"
         verbose_name_plural = "Фото Лодок"
+
+class Reviews(models.Model):
+    """Отзывы"""
+    email = models.EmailField()
+    name = models.CharField("Имя", max_length=100)
+    text = models.TextField("Сообщение", max_length=5000)
+    parent = models.ForeignKey(
+        'self', verbose_name="Родитель", on_delete=models.SET_NULL, blank=True, null=True
+    )
+    product = models.ForeignKey(Boat, verbose_name="Лодка", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.product}"
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+
+class VideosBoats(models.Model):
+    video = models.CharField("Ссылка на видеоролик", max_length=100)
+    boat = models.ForeignKey(Boat, verbose_name="Лодка", on_delete= models.CASCADE, null=True)
+    class Meta:
+        verbose_name = "Видео"
+        verbose_name_plural = "Видео"
