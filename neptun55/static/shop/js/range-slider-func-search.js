a1ac335bf6a636246f9b71c489aa29fdc30ea0cf
Vitalygorkov <vet55omsk@gmail.com>
2022-03-16 14:21:07 +0600

    0.4

--- neptun55/static/shop/js/range-slider-func-search.js
+++ neptun55/static/shop/js/range-slider-func-search.js
@@ -75,18 +75,6 @@ if (rangeSlider2) {
   });
 }
 
-
-var params = window.location.search.replace( '?', '').split('&').reduce(
-        function(p,e){
-            var a = e.split('=');
-            p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
-            return p;
-        },
-        {}
-    );
-console.log(params.price__lt);
-
-
 // ширина
 const rangeSliderWidth = document.getElementById('range-slider-width');
 if (rangeSliderWidth) {
