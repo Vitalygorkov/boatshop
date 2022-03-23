1fe90e659eeaced7105a096e82c92e568580a3a7
Vitalygorkov <vet55omsk@gmail.com>
2022-03-23 12:08:01 +0600

    0,5

---
+++ neptun55/static/shop/js/slick-script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('.slider').slick({
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     arrows: true,
+     fade: true,
+     asNavFor: '.slider-nav'
+    });
+});
+
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider',
+  dots: false,
+  centerMode: true,
+  arrows: true,
+  focusOnSelect: true
+});
+
+//
+//$(document).ready(function(){
+//    $('.slider').slick({
+//  slidesToShow: 1,
+//  slidesToScroll: 1,
+//  arrows: false,
+//  fade: true,
+//  asNavFor: '.slider'
+//});
+//$('.slider').slick({
+//  slidesToShow: 3,
+//  slidesToScroll: 1,
+//  asNavFor: '.slider',
+//  dots: true,
+//  centerMode: true,
+//  focusOnSelect: true
+//});
