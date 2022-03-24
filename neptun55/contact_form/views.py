71bac46f5fb9a630069af700286cc9243b116fd3
Vitalygorkov <vet55omsk@gmail.com>
2022-03-24 23:47:21 +0600

    0,5

--- neptun55/contact_form/views.py
+++ neptun55/contact_form/views.py
@@ -15,13 +15,21 @@ class ContactFormView(CreateView):
     form_class = ContactForm
     success_url = "/success/"
     # success_url = reverse_lazy('success_page')
-
     def form_valid(self, form):
         # Формируем сообщение для отправки
         data = form.data
-        subject = f'Сообщение с сайта от {data["first_name"]} {data["last_name"]}, Телефон: {data["phone"]} Почта отправителя: {data["email"]}'
-        email(subject, data['message'] + 'Телефон: ' + data['phone'])
-        return super().form_valid(form)
+        if data["last_name"] == '':
+            subject = f'Сообщение с сайта от {data["first_name"]} {data["last_name"]}, Телефон: {data["phone"]} Почта отправителя: {data["email"]}'
+            email(subject, data['message'] + 'Телефон: ' + data['phone'])
+            return super().form_valid(form)
+        else:
+            print('last name input')
+            print(str(data["last_name"]))
+            return super().form_valid(form)
+        # data = form.data
+        # subject = f'Сообщение с сайта от {data["first_name"]} {data["last_name"]}, Телефон: {data["phone"]} Почта отправителя: {data["email"]}'
+        # email(subject, data['message'] + 'Телефон: ' + data['phone'])
+        # return super().form_valid(form)
 
 # Функция, которая вернет сообщение в случае успешного заполнения формы
 def success(request):
