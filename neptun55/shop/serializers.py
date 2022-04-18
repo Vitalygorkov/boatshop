14b94ee7fdf4b22369cb0fb5e7334090d75e3121
Vitalygorkov <vet55omsk@gmail.com>
2022-04-19 00:55:57 +0600

    0.60

--- neptun55/shop/serializers.py
+++ neptun55/shop/serializers.py
@@ -1,13 +1,17 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from shop.models import Product, Category
 
 
 class ProductSerializer(ModelSerializer):
+    product_abs_url = serializers.SerializerMethodField()
     class Meta:
         model = Product
         # fields = ['name', 'image', 'price', 'slug']
         fields = '__all__'
+    def get_product_abs_url(self, obj):
+        return obj.get_absolute_url()
 
 class CategorySerializer(ModelSerializer):
     class Meta:
