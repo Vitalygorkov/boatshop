acc414ae8a52267bb9e65f361b16364d60922bd1
Vitalygorkov <vet55omsk@gmail.com>
2022-04-27 11:49:19 +0600

    0.63

--- neptun55/shop/serializers.py
+++ neptun55/shop/serializers.py
@@ -1,13 +1,22 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 
-from shop.models import Product, Category
+from shop.models import Product, Category, Boat
 
 
 class ProductSerializer(ModelSerializer):
     product_abs_url = serializers.SerializerMethodField()
     class Meta:
         model = Product
+        fields = ['name', 'image','manufacturer', 'price', 'sale', 'color', 'recommendations', 'accessories', 'category', 'slug']
+        # fields = '__all__'
+    def get_product_abs_url(self, obj):
+        return obj.get_absolute_url()
+
+class BoatSerializer(ModelSerializer):
+    product_abs_url = serializers.SerializerMethodField()
+    class Meta:
+        model = Boat
         # fields = ['name', 'image', 'price', 'slug']
         fields = '__all__'
     def get_product_abs_url(self, obj):
