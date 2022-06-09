99d22064d1404aadc8f155d1086306f15d995ad2
Vitalygorkov <vet55omsk@gmail.com>
2022-06-09 10:42:51 +0600

    add parse_url in models.py

--- neptun55/contact_form/models.py
+++ neptun55/contact_form/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Contact(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200, blank=True)
-    phone = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200, null=True, blank= True,)
     locality = models.CharField(max_length=200, blank=True)
     email = models.CharField(max_length=200, blank=True)
     message = models.TextField(max_length=1000)
